af8021f425d06796b6bf968f3344870ce7f723a9
Anadnaifan <anandaifan65@gmail.com>
2024-04-08 02:09:42 +0700

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
+import StoreIcon from '@mui/icons-material/Store';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+    <div className="top">
+      <Link to="/">
+        <span className="logo">Store</span>
+      </Link>
+    </div>
+    <hr />
+    <div className="center">
+      <ul>
+        <p className="title">MAIN</p>
+        <li>
+          <DashboardIcon className='icon'></DashboardIcon>
+          <span>Dashboard</span>
+        </li>
+        <p className="title">LIST</p>
+        <Link to="/users">
+          <li>
+           <PersonOutlineOutlinedIcon className='icon'></PersonOutlineOutlinedIcon>
+            <span>Users</span>
+          </li>
+        </Link>
+        <Link to="/products">
+          <li>
+          <StoreIcon className='icon'></StoreIcon>
+            <span>Products</span>
+          </li>
+        </Link>
+        <li>
+        <ShoppingCartTwoToneIcon className='icon'></ShoppingCartTwoToneIcon>
+          <span>Orders</span>
+        </li>
+        <p className="title">USER</p>
+        <li>
+        <AccountBoxIcon className='icon'></AccountBoxIcon>
+          <span>Profile</span>
+        </li>
+        <li>
+        <LogoutIcon className='icon'></LogoutIcon>
+          <span>Logout</span>
+        </li>
+      </ul>
+    </div>
+    <div className="bottom">
+      <div className="colorOption"></div>
+      <div className="colorOption"></div>
+    </div>
+  </div>
+    );
+};
+
+export default Sidebar;
